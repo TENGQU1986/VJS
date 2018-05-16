9060988376c9c1ac65c7c24296dea69f13a561e0
Patrick Qu <quteng.bo@gmail.com>
2018-05-16 16:10:59 -0400

    add remove

--- scratch/app.js
+++ scratch/app.js
@@ -10,10 +10,44 @@ loadEventListeners();
 
 //Load all event listeners
 function loadEventListeners() {
+  //dom load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   //Add Task event
   form.addEventListener('submit', addTask);
   //remove task event
-  form.addEventListener('click', removeTask);
+  taskList.addEventListener('click', removeTask);
+  //clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //filter
+  filter.addEventListener('keyup', filtertasks);
+}
+
+//get tasks from LS
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+
+  li.className = 'collection-item';
+
+  li.appendChild(document.createTextNode(task));
+
+  const link = document.createElement('a');
+
+  link.className = 'delete-item secondary-content';
+
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+  });
 }
 
 
@@ -39,6 +73,8 @@ function addTask(e) {
   li.appendChild(link);
 
   taskList.appendChild(li);
+  //store in LS
+  storeTaskInLocalStorage(taskInput.value);
 
   taskInput.value = '';
 
@@ -47,9 +83,40 @@ function addTask(e) {
 
 }
 
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};  
+
+
+
 function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
-    console.log(e.target);
+    e.target.parentElement.parentElement.remove();
   }
 }
 
+function clearTasks(e) {
+  if(e.target.classList.contains('clear-tasks')) {
+    document.querySelectorAll('ul.collection .collection-item').forEach((item) => item.remove());
+  }
+}
+
+function filtertasks(e) {
+  const text = e.target.value;
+  const listItemsArr = Array.from(document.querySelectorAll('ul.collection .collection-item'));
+  listItemsArr.forEach((task) => {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
